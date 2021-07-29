daac4c61bdcbc7822980ebd2b38ae80ea668bff3
Pradeep8 <Pradeep8@Pradeep>
2021-07-29 19:26:57 +0530

    First Commit

---
+++ JavaSession/src/basics/Samplemethod.java
@@ -0,0 +1,30 @@
+package basics;
+
+public class Samplemethod {
+	
+	public void A() {
+		
+		System.out.println("Welcome to Automation Testing 1");
+	}
+	
+	public void B() {
+		
+		System.out.println("Welcome to Automation Testing 2");
+	}
+
+	public void C() {
+	
+	System.out.println("Welcome to Automation Testing 3");
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Samplemethod obj = new Samplemethod();
+		//obj.A();
+		obj.B();
+		//obj.C();
+
+	}
+
+}
